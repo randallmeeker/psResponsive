1e055b435c84714b9892b0bbddf8a2acd50f5662
George <randallmeeker@gmail.com>
2013-11-22 17:04:06 -0700

    Update ps-responsive.js
    code cleanup

--- ps-responsive.js
+++ ps-responsive.js
@@ -1,72 +1,80 @@
 angular.module('psResponsive', [])
-.value('psResponsiveConfig', {
-        sizes : [
-		{name:'tiny', minWidth: 0},
-		{name:'small', minWidth: 768},
-		{name:'medium', minWidth: 992},
-		{name:'large', minWidth: 1200}
-	]
+    .value('psResponsiveConfig', {
+        sizes: [{
+            name: 'tiny',
+            minWidth: 0
+        }, {
+            name: 'small',
+            minWidth: 768
+        }, {
+            name: 'medium',
+            minWidth: 992
+        }, {
+            name: 'large',
+            minWidth: 1200
+        }]
     })
-.factory('$psResponsive', ['$window', '$filter', '$rootScope', 'psResponsiveConfig', 
-            function($window, $filter, $rootScope, psResponsiveConfig){
-	
+    .factory('psResponsive', ['$window', '$filter', '$rootScope', 'psResponsiveConfig',
+        function($window, $filter, $rootScope, psResponsiveConfig) {
 
-	var opRegEx = /[<>]\s\w+/
-	var forEach = angular.forEach,
-		filter = angular.filter,
-		sizes = psResponsiveConfig.sizes,
-		sizes = $filter('orderBy')(sizes, '-minWidth')
 
-	var getHeight = function(){
-		return $window.innerHeight
-		},
+            var opRegEx = /[<>]\s\w+/
+            var forEach = angular.forEach,
+                filter = angular.filter,
+                sizes = psResponsiveConfig.sizes,
+                sizes = $filter('orderBy')(sizes, '-minWidth')
 
-		getWidth = function(){
-			return $window.innerWidth
-		},
+                var getHeight = function() {
+                    return $window.innerHeight
+            },
 
-		getLabel = function(){
-			var cWidth = getWidth(),
-				returnVal = false;
-				for(var i=0;i<sizes.length;i++){
-					if(parseInt(cWidth) >= parseInt(sizes[i].minWidth)){
-						return sizes[i].name
-					}
-				}
-			},
-		getWidthFromLabel = function(label){
-            return $filter('filter')(sizes, {name:label})[0]["minWidth"];
-		},
-		
-		getTest = function(test){
-            var thingy = test.split(' ')[0],
-                against = test.split(' ')[1];
-           
-            if( isNaN(against) ){
-                console.log('(' + getWidth()+thingy+getWidthFromLabel(against) + ')')
-                return eval('(' + getWidth()+thingy+getWidthFromLabel(against) + ')');
-            } else {
-                return eval('(' + getWidth()+thingy+parseInt(against) + ')');
+                getWidth = function() {
+                    return $window.innerWidth
+                },
+
+                getLabel = function() {
+                    var cWidth = getWidth(),
+                        returnVal = false;
+                    for (var i = 0; i < sizes.length; i++) {
+                        if (parseInt(cWidth) >= parseInt(sizes[i].minWidth)) {
+                            return sizes[i].name
+                        }
+                    }
+                },
+                getWidthFromLabel = function(label) {
+                    return $filter('filter')(sizes, {
+                        name: label
+                    })[0]["minWidth"];
+                },
+
+                getTest = function(test) {
+                    var thingy = test.split(' ')[0],
+                        against = test.split(' ')[1];
+
+                    if (isNaN(against)) {
+                        return eval('(' + getWidth() + thingy + getWidthFromLabel(against) + ')');
+                    } else {
+                        return eval('(' + getWidth() + thingy + parseInt(against) + ')');
+                    }
+                }
+
+            angular.element($window).on('resize', function() {
+                $rootScope.$digest();
+            })
+
+            return function(onwha) {
+                if (!onwha) {
+                    return getLabel()
+                } else if (onwha == 'width') {
+                    return getWidth();
+                } else if (onwha == 'height') {
+                    return getHeight();
+                } else if (opRegEx.test(onwha)) {
+                    return getTest(onwha);
+                } else {
+                    return (getLabel() == onwha)
+                }
+                return false;
             }
-                
-		}
-
-		angular.element($window).on('resize', function(){
-			$rootScope.$digest();
-		})    
-
-	return function(onwha){
-		if(!onwha){
-			return getLabel()
-		} else if (onwha == 'width'){
-			return getWidth();
-		} else if (onwha == 'height'){
-			return getHeight();
-		} else if (opRegEx.test(onwha)){
-            return getTest(onwha);
-		} else {
-			return (getLabel() == onwha)
-		}
-		return false;
-	}
-}])
+        }
+    ])
